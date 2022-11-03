dfeab470b9727ec1c901067c0ea217658c621307
Pankaj Bhattarai <68217624+im-endangered@users.noreply.github.com>
2022-11-03 06:51:44 +0530

    finishing touch

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ def submit():
     convertedGrade=[int(cn),int(ebs),int(dme),int(eds),int(fla),int(efs),int(cps2),int(msit)]
     sgpa=(convertedGrade[0]*4+convertedGrade[1]*3+convertedGrade[2] *4+convertedGrade[3]*3 +convertedGrade[4] *3+convertedGrade[5] *3 +convertedGrade[6] *1 +convertedGrade[7]*1)/22   
     return redirect(url_for('result',score=sgpa))
-    # return convertedGrade
-    
+   
 if __name__=='__main__':
     app.run(debug=True)
